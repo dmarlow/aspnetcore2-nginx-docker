1baf9e2cdd843c947665b7605efabca2f2b72f02
Dariel Marlow <dmarlow@gmail.com>
2017-11-01 11:23:29 -0700

    Initial commit

---
+++ src/api/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreLinux.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var name = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
+                Environment.GetEnvironmentVariable("HOSTNAME") ??
+                Environment.MachineName;
+            return new[] { $"MachineName={name}" }.Union(Request.Headers.Select(x => $"{x.Key}={x.Value}"));
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
